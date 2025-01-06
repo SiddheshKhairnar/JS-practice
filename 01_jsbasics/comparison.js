de1a06db65824a5f79408ca74bb865bcc4424666
SiddheshKhairnar <89126808+SiddheshKhairnar@users.noreply.github.com>
2025-01-06 06:46:31 +0000

    commit success

---
+++ 01_jsbasics/comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>1)
+// console.table([2<=1,1==1, 2!=1])
+
+// console.log(3=="3")
+// console.log(3==="3")
+
+console.log(null==0)
+console.log(null >=0)
+console.log(null>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
