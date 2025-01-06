249ad5b46562d9eea1ba222cfb6f68c490be6255
SiddheshKhairnar <89126808+SiddheshKhairnar@users.noreply.github.com>
2025-01-06 06:47:30 +0000

    changed

--- 01_jsbasics/conversionops.js
+++ 01_jsbasics/conversionops.js
@@ -1,5 +1,5 @@
 let score = "33ws"
-console.log(typeof score);
+// console.log(typeof score);
 
 let valuebeInNumber = Number(score)
 // console.log(typeof valuebeInNumber)
@@ -7,4 +7,23 @@ let valuebeInNumber = Number(score)
 
 let IsLoggedIn = 1
 let Booleanvalue = Boolean(IsLoggedIn)
-console.log(Booleanvalue)
+// console.log(Booleanvalue)
+
+let someName = 33
+// console.log(typeof someName)
+let someString = String(someName)
+// console.log(typeof someString)
+
+
+// **********Operations*************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(1+2) //3
+console.log(typeof ("1"+2)) //12
+console.log(1+2+"2") //32
+console.log("1"+2+3) //123
+
+ 
