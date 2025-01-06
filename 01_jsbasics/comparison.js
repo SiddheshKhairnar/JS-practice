283394bfa5a989d1f8fb166e4334966f84fcd942
SiddheshKhairnar <89126808+SiddheshKhairnar@users.noreply.github.com>
2025-01-06 10:44:33 +0000

    files committed

--- 01_jsbasics/comparison.js
+++ 01_jsbasics/comparison.js
@@ -4,9 +4,33 @@
 // console.log(3=="3")
 // console.log(3==="3")
 
-console.log(null==0)
-console.log(null >=0)
-console.log(null>0)
-console.log(undefined==0)
-console.log(undefined>=0)
-console.log(undefined>0)
+// console.log(null==0)
+// console.log(null >=0)
+// console.log(null>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined>0)
+
+//******* Strings concept *********
+
+let name = "Shyam"
+let age=12
+console.log(`My name is ${name} and I am ${age} years old`)
+
+
+const name1 = "hiteshc_12"
+// const newstring = name1.substring(0,6)
+// console.log(newstring);
+
+const anotherstring = name1.slice(-5,-2)
+console.log(anotherstring);
+
+
+const newOne = "     Sid    "
+console.log(newOne.trim())
+
+const url="www.khairnar_40%.com"
+console.log(url.replace('40%','20%'))
+
+//trim,replace,slice,substring are important string methods in js
+
---
+++ 01_jsbasics/nums_maths.js
@@ -0,0 +1,18 @@
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+let otherNumber = 1321.22
+
+// console.log(otherNumber.toPrecision(5))
+
+// console.log(Math)
+// console.log(Math.abs(-8))
+// console.log(Math.round(12.33))
+// console.log(Math.ceil(5.3))
+// console.log(Math.floor(12.45))
+
+console.log(Math.random()) //give the random between 0 to 1
+console.log(Math.random()*10) //give the value between 1 to 9
+console.log(Math.floor(Math.random()*10 +1)) //give the value between and add 1 to it by doing floor into it
